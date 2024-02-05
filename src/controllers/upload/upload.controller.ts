224fe5d1c5b7917748148ded2b95f3c7da644960
tuanama00 <jr.araujot@gmail.com>
2024-02-05 00:49:00 -0500

    Upload Ok con validacion

--- src/controllers/upload/upload.controller.ts
+++ src/controllers/upload/upload.controller.ts
@@ -1,13 +1,22 @@
 import {Request, Response} from 'express';
 import {PrismaClient} from '@prisma/client';
 
+
 const prisma = new PrismaClient();
 
 const uploadMethod = async (req: Request, res: Response) => {
     try {
         const {file} = req;
-       // console.log("mostrando file", file)
-       // console.log(file);
+        const value = {
+            path: file?.path || '',
+            filename: file?.filename || '',
+            size: file?.size || 0,
+            mimetype: file?.mimetype || ''
+        };
+
+        const result = await prisma.files.create({
+            data: value
+        });
         return res.status(200).json({data: file})
     } catch (error) {
         console.log("error::controller::upload", error);
@@ -17,18 +26,7 @@ const uploadMethod = async (req: Request, res: Response) => {
 
 
 
-const postMethod = async (req: Request, res: Response) => {
-    try{
-        const {body} = req;
-        const result = await prisma.productos.create({
-            data: body
-        });
-        res.status(200).json(result);
-    } catch (e) {
-        console.log("error:controller::productos", e);
-        return res.status(500).json(e);
-    }
-}
+
 // const putMethodById = async (req: Request, res: Response) => {
 //     const {id} = req.params;
 //     const {body} = req;
--- src/middlewares/upload/index.ts
+++ src/middlewares/upload/index.ts
@@ -1,7 +1,8 @@
 import multer, {MulterError} from 'multer';
 import {Request, Response, NextFunction} from 'express';
 import path from 'path';
-import { format } from 'date-fns';
+import {format} from 'date-fns';
+
 
 const uploadPath = path.join(__dirname, '../../uploads');
 
@@ -18,29 +19,33 @@ const storage = multer.diskStorage({
 });
 
 
-const filter = ( req: Request, file: Express.Multer.File, cb: Function) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' ||
+const filter = (req: Request, file: Express.Multer.File, cb: Function) => {
+    if (file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'image/png' ||
         file.mimetype === 'application/pdf' ||
         file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
         file.mimetype === 'application/zip' ||
-        file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'||
-        file.mimetype === 'application/zip') {
+        file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    ){
+        //console.log(file.size);
         cb(null, true);
-       // console.log(file.mimetype);
+        // console.log(file.mimetype);
     } else {
-        cb(new Error('File type not supported'), false);
-       // console.log(file.mimetype);
+        cb(new Error('Archivo no soportado'), false);
+        // console.log(file.mimetype);
     }
 }
 
 const upload = multer({
     storage: storage,
-    fileFilter: filter
+    fileFilter: filter,
+    limits: {fileSize: 10 * 1024 * 1024}
 }).single('file');
 
 const uploadMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    upload(req, res, (err: any)=>{
-        if(err) return res.status(500).json({error: err.message});
+    upload(req, res, (err: any) => {
+        if (err) return res.status(500).json({error: err});
+       // console.log(err);
         next();
     })
 }
